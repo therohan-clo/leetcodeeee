50065c912bee35501e939cec7212a7c6d9f91929
Rohan Pratik Choudhary <choudhary.rp04@gmail.com>
2026-07-11 10:52:57 +0545

    Time: 0 ms (100%), Space: 7.9 MB (1.48%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        while(b != 0){
-            int carry= a; 
-            a = a ^ b;
-            b = (carry & b)<<1;
-        }
+         while (b != 0){
+            int carry= a  ; //# Find common set bits
+            a = a ^ b      ;//# Add without carrying
+            b = (carry & b) << 1 ; //# Shift carry to the left
+    }
     return a;
     }
 };
